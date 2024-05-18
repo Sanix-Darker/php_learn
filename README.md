8b6a4d242420a82b1fecf01c7b6403fd13581daa
sanix-darker <s4nixd@gmail.com>
2024-05-19 01:03:07 +0200

    feat: playground and README

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 ## DATA TYPES
 
 ```php
+// Variables and data types :
+
 $str = "Hello";
 $bool = true; // or false
 $int = 12;
 $float = 12.12;
 
-$arr = array('Hello', 'there');
+$is_empty = NULL;
+
+$arr = array('Hello', 12, 'there', false);
+var_dump($arr); //to print the whole content
+
+$hash_map = array(
+    'x' => 12, 'y' => 33, 'z' => $arr
+);
+var_dump($hash_map); // we can also use print_r, it's same but less infos.
 ```
--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <?php
     // this is a test comment
-    $varx = "sanix";
-    $some_bool = true;
+    /* $varx = "sanix"; */
+    /* $some_bool = true; */
 
-    echo "Hello there ".$varx." !\n";
+    /* echo "Hello there ".$varx." !\n"; */
 
-    function add($a, $b) {
+    /* function add($a, $b) { */
 
-        if ($some_bool){
-            $result = $a + $b;
-        }
+    /*     if ($some_bool){ */
+    /*         $result = $a + $b; */
+    /*     } */
 
-        return $result;
-    }
+    /*     return $result; */
+    /* } */
 
-    echo add(10, 22);
+    /* echo add(10, 22); */
+
+$str = "Hello";
+$bool = true; // or false
+$int = 12;
+$float = 12.12;
+
+$arr = array('Hello', 12, $bool, 'there');
+var_dump($arr); // to print the whole content
+
+$hash_map = array(
+    'x' => 12, 'y' => 33, 'z' => $arr
+);
+print_r($hash_map);
+
 ?>
