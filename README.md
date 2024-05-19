35dea3829f9857403a7b46e296c8428c27232f52
sanix-darker <s4nixd@gmail.com>
2024-05-19 18:11:53 +0200

    feat: update on syntax

--- README.md
+++ README.md
@@ -21,3 +21,85 @@ $hash_map = array(
 );
 var_dump($hash_map); // we can also use print_r, it's same but less infos.
 ```
+
+## CONDITIONS
+
+- if / else:
+
+```php
+if ($user == "this" AND $pass == "xxx") { // AND (&&) | OR (||)
+    echo "this";
+ }else{
+    echo "that";
+}
+```
+
+- switch /case:
+
+```php
+const name_r = "Hlllo";
+switch (strlen(name_r)) {
+    case 5:
+        echo "IT's 5";
+        break;
+    default:
+        echo "OUPS !!!";
+        break;
+}
+```
+
+- ternary:
+
+```php
+echo ($content_radian > 10) ? "is upper than 10" : "is lower than 10";
+```
+
+## LOOP
+
+- foreach
+```php
+$items = ['this', 10, 'that']; // array('', ...) works too
+
+foreach ($items as $item){
+    echo $item;
+}
+```
+- while
+
+```php
+$i = 0;
+while($i < 10){
+    // do something
+    echo "count - ".$i;
+    $i += 1;
+}
+```
+
+- do/while (execute first before checking the condition).
+```php
+$i = 0;
+do {
+    echo "--{$i}";
+    $i -= 1;
+}while($i > 3);
+```
+
+- for
+```php
+for ($i = 0; $i < 10 &i ++;){
+    echo $i;
+}
+```
+
+## FORMATING
+
+```php
+user="dk";
+echo "${user} is connected !";
+```
+
+## EXTRAS
+
+```php
+strlen(var); // to get size of a string
+```
--- index.php
+++ index.php
@@ -1,36 +1,79 @@
 <?php
-
+// constant
 define("EXAMPLE_CONSTANT", 12);
 
-    // this is a test comment
-    /* $varx = "sanix"; */
-    /* $some_bool = true; */
+// vars
+// $str = "Hello";
+// $bool = true; // or false
+// $int = 12;
+// $float = 12.12;
+//
+// $arr = array('Hello', 12, $bool, 'there');
+// var_dump($arr); // to print the whole content
+//
+// $hash_map = array(
+//     'x' => 12, 'y' => 33, 'z' => $arr
+// );
+// print_r($hash_map);
+//
+// print_r(EXAMPLE_CONSTANT);
 
-    /* echo "Hello there ".$varx." !\n"; */
+const required = 11;
+echo required;
 
-    /* function add($a, $b) { */
+$user="dk";
+const pass = "this";
+echo "'{$user}' is connected with : `".pass."`";
+echo "";
+echo "\nSize of pass is : ".strlen(pass)."\n";
 
-    /*     if ($some_bool){ */
-    /*         $result = $a + $b; */
-    /*     } */
+const name_r = "Hlllo";
 
-    /*     return $result; */
-    /* } */
+switch (strlen(name_r)) {
+    case 5:
+        echo "IT's 5";
+        break;
+    default:
+        echo "OUPS !!!";
+        break;
+}
 
-    /* echo add(10, 22); */
+echo "\n";
+$content_radian = 11;
+echo ($content_radian > 10) ? "is upper than 10" : "is lower than 10";
 
-$str = "Hello";
-$bool = true; // or false
-$int = 12;
-$float = 12.12;
+//$items = ['this', 10, 'that']; // array('', ...) works too
+//
+//foreach ($items as $item){
+//    echo ">>.item: ".$item."\n";
+//}
+//
+//$i = 0;
+//while($i < 5){
+//    // do something
+//    echo "\ncount - ".$i;
+//    $i += 1;
+//}
 
-$arr = array('Hello', 12, $bool, 'there');
-var_dump($arr); // to print the whole content
+echo "\n";
+$i = 10;
+do {
+    echo "--{$i}\t";
+    $i -= 1;
+}while($i > 3);
 
-$hash_map = array(
-    'x' => 12, 'y' => 33, 'z' => $arr
-);
-print_r($hash_map);
+echo "\n";
+for ($i = 0; $i < 10; $i+=2){
+    echo "+++".$i;
+}
 
-print_r(EXAMPLE_CONSTANT);
+// yes typing
+function displayName(string $name, int $age): int {
+    echo "Hello there !";
+    echo "\n{$name} is {$age} year old !";
+    return 1;
+}
+
+echo "\n";
+echo displayName("sanix", 30) == 1 ? "\nooh": NULL;
 ?>
